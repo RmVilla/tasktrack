a83e75120bcbe9b64c3ba60215877a1d38cb7864
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-19 08:27:43 +0800

    - Add Basic Functionalities

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'due_date', 'is_completed')
+    search_fields = ('title', 'user__username')
+    list_filter = ('is_completed', 'due_date')
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasktrack/task_list.html', {'tasks': tasks})
+
+
+@login_required
+def create_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        Task.objects.create(
+            title=title,
+            description=description,
+            due_date=due_date,
+            user=request.user
+        )
+        return redirect('task_list')
+    return render(request, 'tasktrack/create_task.html')
+
+
+@login_required
+def mark_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    task.is_completed = True
+    task.save()
+    return redirect('task_list')
+
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    task.delete()
+    return redirect('task_list')
